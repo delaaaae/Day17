44c111e32fb94482fa8a65856c10e54dbd6c00cf
unknown <delic.amir09@gmail.com>
2021-03-25 09:52:46 +0100

    Initial import

---
+++ ex08/src/index.js
@@ -0,0 +1,15 @@
+const _ = require('lodash');
+
+
+function findUser(lastName, gender) {
+    try {
+
+    } catch (error) {
+        return
+        console.log();
+    }
+}
+
+getUser();
+findUser();
+module.exports = findUser;
